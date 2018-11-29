504eb738a134c50aa45d7d34722982daea4e782f
Rob Thatcher <thatcr@gmail.com>
2018-11-29 23:26:09 +0000

    router for stream -> OutputDebugString

---
+++ xll/output.py
@@ -0,0 +1,15 @@
+from cffi import FFI
+
+ffi = FFI()
+ffi.cdef("""
+   void OutputDebugStringW(const wchar_t*);
+""")
+kernel32 = ffi.dlopen('kernel32.dll')
+
+class OutputDebugStringWriter:
+    def write(self, value):
+        kernel32.OutputDebugStringW(value.rstrip())
+
+    def flush(self):
+        pass
+
