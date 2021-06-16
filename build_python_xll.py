aeb2dea031719e0d44b6e7a1dcb9ad7543a680ff
Rob Thatcher <thatcr@gmail.com>
2021-06-16 18:06:26 +0100

    wip: port to 64-bit

--- build_python_xll.py
+++ build_python_xll.py
@@ -21,31 +21,33 @@ ffi.cdef('''
         DWORD	AddressOfNameOrdinals;
     } IMAGE_EXPORT_DIRECTORY,*PIMAGE_EXPORT_DIRECTORY;
     
-    DWORD                   pRVABase;
     IMAGE_EXPORT_DIRECTORY* pExportDirectory;
     
+    // these convert to/from the DWORD offsets in the export table
     void* PointerFromAddress(DWORD Address);    
     DWORD AddressFromPointer(void* Pointer);          
 ''')
 
 ffi.embedding_api('''      
-    extern "Python" int __stdcall xlAutoOpen(void);
-    extern "Python" int __stdcall xlAutoClose(void);
-    extern "Python" int __stdcall xlAutoAdd(void);
-    extern "Python" int __stdcall xlAutoRemove(void);
+    extern "Python" int xlAutoOpen(void);
+    extern "Python" int xlAutoClose(void);
+    extern "Python" int xlAutoAdd(void);
+    extern "Python" int xlAutoRemove(void);
     
-    extern "Python" void __stdcall xlAutoFree12(LPXLOPER12);
+    extern "Python" void xlAutoFree12(LPXLOPER12);
     
-    extern "Python" LPXLOPER12 __stdcall _xlfCaller();
-    extern "Python" LPXLOPER12 __stdcall py_eval(const char* expression);             
+    extern "Python" LPXLOPER12 _xlfCaller();
+    extern "Python" LPXLOPER12 py_eval(const char* expression);             
 ''')
 
 
 ffi.set_source("_python_xll", r"""
+#undef NDEBUG
 #include <assert.h>
 #include <WINDOWS.H>
 #include <XLCALL.H>
 
+#ifdef _M_IX86
 #pragma comment(linker, "/export:xlAutoOpen=_xlAutoOpen@0")
 #pragma comment(linker, "/export:xlAutoClose=_xlAutoClose@0")
 #pragma comment(linker, "/export:xlAutoAdd=_xlAutoAdd@0")
@@ -54,20 +56,22 @@ ffi.set_source("_python_xll", r"""
 
 #pragma comment(linker, "/export:py.eval=_py_eval@4")
 #pragma comment(linker, "/export:xlfCaller=__xlfCaller@0")
+#endif
 
 
-extern DWORD pRVABase = 0;
+// use a pointer to bytes so that the offset arithmetic works
+static LPBYTE pRVABase = 0; 
+
 IMAGE_EXPORT_DIRECTORY* pExportDirectory = 0; 
 
 extern void* PointerFromAddress(DWORD Address)
 {
-    return (void*)(Address + pRVABase);    
+    return (void*)(pRVABase + Address);    
 }
-
 
 extern DWORD AddressFromPointer(void* Pointer)
 {
-    return (DWORD)(Pointer) - pRVABase;    
+    return (DWORD) (((LPBYTE) Pointer) - pRVABase);    
 }
 
 // initialize this from the 
@@ -75,31 +79,34 @@ extern void** exports= 0;
 
 void _set_export_directory(HINSTANCE hInstDll)
 {            
-    // figure out the location of the export able, this is easier than 
-    // using python
-    pRVABase = (DWORD) hInstDll;
+    // figure out the location of the export table, this is easier than 
+    // using python and working around type/pointer casts in cffi
+    pRVABase = (void*) hInstDll;
     
     IMAGE_DOS_HEADER* pDosHeader = 
         (IMAGE_DOS_HEADER*) hInstDll;
-    assert(pDosHeader->e_magic == 0x4550);    
-    
-    IMAGE_NT_HEADERS32* pNtHeaders32 = 
-        (IMAGE_NT_HEADERS32*) (pDosHeader->e_lfanew + pRVABase);
-    assert(pNtHeaders32->Signature == 0x4550);
-    
-    assert(pNtHeaders32->OptionalHeader.DataDirectory[0].VirtualAddress != 0);
-    assert(pNtHeaders32->OptionalHeader.DataDirectory[0].Size != 0);
+    assert(pDosHeader->e_magic == 0x5A4D);             
+
+    IMAGE_NT_HEADERS* pNtHeaders = 
+        (IMAGE_NT_HEADERS*) (pDosHeader->e_lfanew + pRVABase);    
+       
+    assert(pNtHeaders->Signature == 0x4550);
+        
+    assert(pNtHeaders->OptionalHeader.DataDirectory[0].VirtualAddress != 0);
+    assert(pNtHeaders->OptionalHeader.DataDirectory[0].Size != 0);
         
     pExportDirectory = 
         (IMAGE_EXPORT_DIRECTORY*)     
-        (pNtHeaders32->OptionalHeader.DataDirectory[0].VirtualAddress + pRVABase);                             
+        (pNtHeaders->OptionalHeader.DataDirectory[0].VirtualAddress + pRVABase);                             
+    
 }
 
-#define E(n) extern CFFI_DLLEXPORT __declspec(naked) _ ## n() { ExitProcess(0x00FF0000); }
+#define E(n) extern CFFI_DLLEXPORT _ ## n() { ExitProcess(0x00FF0000); }
 
 void _set_python_home()
 {
     CHAR   DllName[MAX_PATH];
+
     sprintf(DllName, "python%d%d.dll", PY_MAJOR_VERSION, PY_MINOR_VERSION);
 
     HMODULE hModule = GetModuleHandle(DllName);
@@ -116,13 +123,18 @@ BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpvReason)
 {                        
     DisableThreadLibraryCalls(hInstDLL);
     
-    if (fdwReason == DLL_PROCESS_ATTACH) {                  
+    if (fdwReason == DLL_PROCESS_ATTACH) {                          
         _set_export_directory(hInstDLL);
+
+        MessageBox(NULL, "Found Export Directory", "Hooray", MB_OK);
         
         if (Py_IsInitialized())
             return TRUE;
 
-        _set_python_home();                
+        _set_python_home(); 
+
+        MessageBox(NULL, "All Done", "Hooray", MB_OK);               
+                
     }
             
     
@@ -140,21 +152,10 @@ from xll.output import OutputDebugStringWriter
 # send all output to the debugging console
 sys.stderr = sys.stdout = OutputDebugStringWriter()
 
-# if we have a PYDEVD port set, then attach to the debugger
-if 'PYDEVD_PORT' in os.environ:
-    import pydevd
-    pydevd.settrace(
-        host=os.getenv('PYDEVD_HOST', 'localhost'),
-        port=int(os.getenv('PYDEVD_PORT')),
-        suspend=False,
-        stdoutToServer=True,
-        stderrToServer=True
-    )
-
 # defer to the python module
 import xll.auto
 """)
 
 if __name__ == '__main__':
-    ffi.compile(target=os.path.join(os.path.dirname(__file__), 'python.xll'))
+    ffi.compile(target=os.path.join(os.path.dirname(__file__), 'python.xll'), debug=False)
 
